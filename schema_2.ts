81cc526fa1bbac7c4fe1f205b54ab9eff731e127
tluwaniMS <tluwani2020@gmail.com>
2022-01-04 14:49:11 +0200

    adding-doctor-and-hospitals-field-types

--- schema_2.ts
+++ schema_2.ts
@@ -1,15 +1,99 @@
 import { list } from "@keystone-6/core";
-import { text } from "@keystone-6/core/fields";
+import { select, text } from "@keystone-6/core/fields";
 import { Lists } from ".keystone/types";
 
 export const lists: Lists = {
   Doctor: list({
-    fields: {}
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      surname: text({ validation: { isRequired: true } }),
+      email: text({ validation: { isRequired: true }, isIndexed: "unique" }),
+      gender: select({
+        type: "enum",
+        options: [
+          { label: "", value: "" },
+          { label: "", value: "" }
+        ]
+      }),
+      specialty: select({
+        type: "enum",
+        options: [
+          { label: "Pediatrician", value: "pediatrician" },
+          { label: "Dematologist", value: "dematologist" },
+          { label: "Cardiologist", value: "cardiologist" },
+          { label: "Urologist", value: "urologist" },
+          { label: "Neurologist", value: "neurologist" },
+          { label: "Psychiatrist", value: "psychiatrist" },
+          { label: "Radiologist", value: "radiologist" }
+        ]
+      }),
+      hospital: select({
+        type: "enum",
+        options: [
+          {
+            label: "Sekhukhune Private Hospital",
+            value: "SKKN"
+          },
+          {
+            label: "Shapo Private Hospital",
+            value: "SHPO"
+          },
+          {
+            label: "Polokwane Hospital",
+            value: "PLKH"
+          },
+          {
+            label: "Botlokwa Hospital",
+            value: "BTLK"
+          },
+          {
+            label: "Marobjyane Private Hospital",
+            value: "MRBJ"
+          },
+          {
+            label: "Ntsundeni Private Hospital",
+            value: "NTNP"
+          },
+          {
+            label: "Moshe Hospital",
+            value: "MSHH"
+          },
+          {
+            label: "Marabastad Hospital",
+            value: "MRSB"
+          },
+          {
+            label: "Baloyi Hospital",
+            value: "BLYH"
+          },
+          {
+            label: "Vhatwanamba Private Health Care",
+            value: "VTPH"
+          },
+          {
+            label: "Bulombo Hospital",
+            value: "BLMH"
+          },
+          {
+            label: "Mashashane Hospital",
+            value: "MSSH"
+          },
+          {
+            label: "dendron Health Centre",
+            value: "DNDH"
+          }
+        ]
+      })
+    }
   }),
   Hospital: list({
-    fields: {}
+    fields: {
+      hospitalName: text({ validation: { isRequired: true } })
+    }
   }),
   Municipality: list({
-    fields: {}
+    fields: {
+      municipalityName: text({ validation: { isRequired: true } })
+    }
   })
 };
