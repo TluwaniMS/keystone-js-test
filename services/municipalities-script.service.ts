1df64f95d6fb28cbc5d405ca5a51c973ef2fe6eb
tluwaniMS <tluwani2020@gmail.com>
2022-01-05 14:05:01 +0200

    adding-logic-to-create-municipalites

--- services/municipalities-script.service.ts
+++ services/municipalities-script.service.ts
@@ -1,1 +1,6 @@
- 
+import { KeystoneContext } from "@keystone-6/core/types";
+import { Municipalities } from "../seed-data/municipalities.data";
+
+export const createMunicipalities = async (context: KeystoneContext) => {
+  await context.query.Municipality.createMany({ data: Municipalities });
+};
