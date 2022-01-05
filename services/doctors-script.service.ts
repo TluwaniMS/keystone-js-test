f24b1e8048700c1109edbb762b5502ac53c9c056
tluwaniMS <tluwani2020@gmail.com>
2022-01-05 14:47:49 +0200

    adding-logic-for-creating-doctors

---
+++ services/doctors-script.service.ts
@@ -0,0 +1,8 @@
+import { KeystoneContext } from "@keystone-6/core/types";
+import { prepareDoctorsData } from "../auxilliary-services/doctors.auxiliary-service";
+import { Doctors } from "../seed-data/doctors.data";
+
+export const createDoctors = async (context: KeystoneContext) => {
+  const preparedDoctors = prepareDoctorsData(Doctors);
+  await context.query.Doctor.createMany({ data: preparedDoctors });
+};
