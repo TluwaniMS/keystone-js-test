347c4a5d7266e828ae8684d9d006ba2089bf2538
tluwaniMS <tluwani2020@gmail.com>
2022-01-10 13:18:50 +0200

    adding-logic-to-

--- schema.ts
+++ schema.ts
@@ -19,7 +19,7 @@ import { list } from "@keystone-6/core";
 
 // We're using some common fields in the starter. Check out https://keystonejs.com/docs/apis/fields#fields-api
 // for the full list of fields.
-import { text, relationship, password, timestamp, select } from "@keystone-6/core/fields";
+import { text, relationship, password, select, checkbox } from "@keystone-6/core/fields";
 // The document field is a more complicated field, so it's in its own package
 // Keystone aims to have all the base field types, but you can make your own
 // custom ones.
@@ -47,7 +47,8 @@ export const lists: Lists = {
         isFilterable: true
       }),
       // The password field takes care of hiding details and hashing values
-      password: password({ validation: { isRequired: true } })
+      password: password({ validation: { isRequired: true } }),
+      isAdmin: checkbox({ defaultValue: false })
     }
   }),
   Doctor: list({
