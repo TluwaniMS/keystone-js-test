70c9e15ea0db1faf1561a61f8bd582f448c2c790
tluwaniMS <tluwani2020@gmail.com>
2022-01-26 10:39:01 +0200

    updating-application-route

--- keystone.ts
+++ keystone.ts
@@ -19,6 +19,7 @@ export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
     // the db sets the database provider - we're using sqlite for the fastest startup experience
+    server: { port: 3005 },
     db: {
       provider: "sqlite",
       url: "file:./keystone.db",
